f3f82cb27593dea96b1475597ae3b6b6ccbe14a3
Steve Chen <steve@arcadiapower.com>
2018-03-04 23:50:03 -0500

    Add token-based authentication with JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
   resources :todos do
     resources :items
   end
